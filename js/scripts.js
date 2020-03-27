9fad099528b3eec2329396ff0797b12794667b24
unknown <macjay314@gmail.com>
2020-03-26 17:45:20 -0700

    remove some user input fields to attempt to debug

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,17 @@
 $(document).ready(function(){
   $("form#user-info").submit(function(event) {
-    var season = $("input:radio[name=season]:checked").val();
-    var dog = $("input:radio[name=dog]:checked").val();
-    var age = parseInt($("input#age").val());
-    var gender = $("select#gender").val();
-    if (age <= 18 || age >= 16 && gender == male){
-
-      $("#d1").show();     
-    }
-    else if (age > 18 && gender === male){
-      $("#d2").show();
-    }
-    else if (age > 18 && dog === "doberman" || dog === "german-shep") {
-      $("#d3").show();
-    }
-    else if (season == fall || winter){
-      $("d4").show();
-    }
-    else if(season === spring || season === summer){
-      $("d5").show();
-    }
     event.preventDefault();
+    var age = parseInt($("#age").val());
+    var gender = parseInt($("#gender").val());
+
+       if (age >= 18 && gender === 1){
+        $("#d3").show();
+      } else {
+        $("#no-input").show();
+      }
+      
+      
+    
+
   });
 });
